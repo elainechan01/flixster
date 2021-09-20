9a78385e544a36a2fed8733d25d5a907bbdb7e32
Elaine <71743181+elainechan01@users.noreply.github.com>
2021-09-20 16:08:14 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Elaine Chan on 9/20/21.
+//  Copyright © 2021 elaine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
