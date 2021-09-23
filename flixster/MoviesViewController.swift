c0e3058e9884d4c56922c1934d9f572d11b5a56e
Elaine <71743181+elainechan01@users.noreply.github.com>
2021-09-23 13:14:01 -0400

    add poster view
    add poster view for each movie using cocoapods and alamofireimage

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -23,6 +24,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af.setImage(withURL: posterUrl!)
         
         return cell
     }
