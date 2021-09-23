1378609b2bfc3d7b5a154005eab89d0128413203
Elaine <71743181+elainechan01@users.noreply.github.com>
2021-09-23 18:01:00 -0400

    add new tab
    add tab to show grid collection view of all related movies to superhero

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Elaine Chan on 9/23/21.
+//  Copyright © 2021 elaine. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
